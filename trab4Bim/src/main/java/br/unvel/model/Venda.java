5d6bf6fbcb8c9a0bfb00f5aa86690d1e78961b11
Cleverson <cleversonhirt@gmail.com>
2015-11-05 21:29:47 -0200

    criado campo jogo em venda

--- trab4Bim/src/main/java/br/unvel/model/Venda.java
+++ trab4Bim/src/main/java/br/unvel/model/Venda.java
@@ -21,6 +21,9 @@ public class Venda implements Serializable
    @Column(name = "version")
    private int version;
 
+   @Column(nullable = false)
+   private String jogo;
+
    public Long getId()
    {
       return this.id;
@@ -39,15 +42,6 @@ public class Venda implements Serializable
    public void setVersion(final int version)
    {
       this.version = version;
-   }
-
-   @Override
-   public String toString()
-   {
-      String result = getClass().getSimpleName() + " ";
-      if (id != null)
-         result += "id: " + id;
-      return result;
    }
 
    @Override
@@ -78,6 +72,25 @@ public class Venda implements Serializable
       final int prime = 31;
       int result = 1;
       result = prime * result + ((id == null) ? 0 : id.hashCode());
+      return result;
+   }
+
+   public String getJogo()
+   {
+      return jogo;
+   }
+
+   public void setJogo(String jogo)
+   {
+      this.jogo = jogo;
+   }
+
+   @Override
+   public String toString()
+   {
+      String result = getClass().getSimpleName() + " ";
+      if (jogo != null && !jogo.trim().isEmpty())
+         result += "jogo: " + jogo;
       return result;
    }
 }
